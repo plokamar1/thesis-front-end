3b8b58e596bf4bec2d96a6a03ab2d90918e8b015
Panagiotis Dorgiakis <p.dorgiak@gmail.com>
2017-09-28 17:46:53 +0300

    Google api initialization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 
 declare const FB: any;
+declare const gapi: any;
 
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
+    public auth2: any;
+
     constructor() {
         //fb.init should start right away. The appId and the version aren't optional
         FB.init({
@@ -16,7 +19,21 @@ export class AppComponent {
                 version: 'v2.10'
             }
         );
-        console.log("Facebook initiated");
+
+    }
+    public googleInit(){
+        gapi.load('auth2', () => {
+            this.auth2 = gapi.auth2.init({
+                client_id: '448479229111-ogop287ptqs9fq6bia40kr7gh2lhg45b.apps.googleusercontent.com',
+                cookiepolicy: 'single_host_origin',
+                scope: 'profile email'
+            });
+        });
+        console.log('Google Initiated');
     }
 
+    ngAfterViewInit(){
+        this.googleInit();
+    }
 }
+
