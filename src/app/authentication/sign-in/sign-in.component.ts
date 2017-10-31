ef1d42dd8241b07a1a5210b45f5594212f53174e
Panagiotis Dorgiakis <p.dorgiak@gmail.com>
2017-10-31 10:18:42 +0200

    Passing the data i get to the data service and navigating the user to the correct url

--- src/app/authentication/sign-in/sign-in.component.ts
+++ src/app/authentication/sign-in/sign-in.component.ts
@@ -1,21 +1,24 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {AfterViewInit, Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {Router} from "@angular/router";
-import {AuthenticationService} from "../authentication.service"
+import {AuthenticationService} from "../../authentication.service"
 import {User} from "../../models/user.model";
+import {DataService} from "../../data.service";
+
 
 @Component({
     selector: 'app-sign-in',
     templateUrl: './sign-in.component.html'
 })
-export class SignInComponent implements OnInit {
+export class SignInComponent implements OnInit{
 
     signInForm: FormGroup;
 
     constructor(private authService: AuthenticationService,
-                private router: Router) {
+                private router: Router,
+                private dataService: DataService) {
         //checking the status of the user. If he is logged in continue
-        authService.checkUserToken('/user-profile');
+        //authService.checkUserToken('/user-profile');
     }
 
     ngOnInit() {
@@ -44,6 +47,7 @@ export class SignInComponent implements OnInit {
                     localStorage.setItem('token', data.token);
                     localStorage.setItem('id', data.id);
                     localStorage.setItem('loginType', 'form');
+
                     this.router.navigateByUrl('/user-profile');
                 },
                 error => {
@@ -62,14 +66,18 @@ export class SignInComponent implements OnInit {
             instance.authService.signUp(response, callsFBUrl)
                 .subscribe(
                     data => {
-                        console.log(data);
+                        instance.dataService.userData = data;
                         localStorage.setItem('token', data.token);
                         localStorage.setItem('id', data.id);
                         localStorage.setItem('loginType', 'facebook');
-                        instance.router.navigateByUrl('user-profile');
+                        instance.router.navigateByUrl('main/profile');
                     },
                     error => console.error(error));
         });
+    }
+
+    onTTRLogin(){
+        this.authService.TTRSignIn();
     }
     onGGLLogin(){
         this.authService.GGLSignIn();
