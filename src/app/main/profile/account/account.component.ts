00335e21da2dd2a300b8301537eb94f54788c73d
Panagiotis Dorgiakis <p.dorgiak@gmail.com>
2017-11-15 19:36:37 +0200

    Added a logout button for every account that the user may have. May drop this scenario . The number of panels that appear with the information for each account is the number of the connected accounts.

--- src/app/main/profile/account/account.component.ts
+++ src/app/main/profile/account/account.component.ts
@@ -1,28 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {DataService} from "../../../data.service";
+import {AuthenticationService} from "../../../authentication.service";
 
 @Component({
-  selector: 'app-account',
-  templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+    selector: 'app-account',
+    templateUrl: './account.component.html',
+    styleUrls: ['./account.component.css']
 })
 export class AccountComponent implements OnInit {
     userData: any;
+    accounts: any;
 
-  constructor(private dataService: DataService) { }
+    constructor(private dataService: DataService,
+                private authService: AuthenticationService) {
+    }
 
-  ngOnInit() {
-      console.log('Initializing account comp');
-      let instance = this;
-      this.dataService.getData()
-          .then(function(response){
-              instance.userData = response;
-              console.log('got Data');
-              console.log(instance.userData);
-          });
 
-      //this.userData = this.dataService.userData;
-      //this.accounts = this.dataService.userData.user_accounts;
-  }
+    logOut(provider: string) {
+        let that = this;
+        switch (provider) {
+            case 'facebook':
+                this.authService.FBLogout().then(function (response) {
+                    console.log(response);
+                    that.authService.FBGetLoginStatus().then(function (response) {
+                       console.log(response);
+                    });
+                });
+        }
+    }
+
+    ngOnInit() {
+        console.log('Initializing account comp');
+        let instance = this;
+        this.dataService.getData()
+            .then(function (response: any) {
+                instance.userData = response;
+                instance.accounts = response.user_accounts[0];
+            });
+
+        //this.userData = this.dataService.userData;
+        //this.accounts = this.dataService.userData.user_accounts;
+    }
 
 }
