4c155e066d4d566560fdc8c25816ea43bffde96d
Panagiotis Dorgiakis <p.dorgiak@gmail.com>
2017-11-20 11:36:35 +0200

    Imported the css for the header. Also i implemented the logout button which clears the user's data from the browser and checks for token which sends him back to sign in. When the user hovers over the buttons of the header the buttons become red. Further changes will probably be made to the style

--- src/app/main/header/header.component.ts
+++ src/app/main/header/header.component.ts
@@ -1,24 +1,34 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {DataService} from "../../data.service";
+import {AuthenticationService} from "../../authentication.service";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css'],
-  encapsulation: ViewEncapsulation.None
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css'],
+    encapsulation: ViewEncapsulation.None
 })
 export class HeaderComponent implements OnInit {
     userData: any;
     userFullname: string;
-  constructor( private dataService: DataService) { }
+    background: string = 'none';
 
-  ngOnInit() {
-      const that = this;
-      this.dataService.getData()
-          .then(function(response){
-              that.userData = response;
-              that.userFullname = that.userData.firstname.concat(' ', that.userData.lastname);
-          });
-  }
+    constructor(private dataService: DataService,
+                private authService: AuthenticationService) {
+    }
+
+    logout(){
+        localStorage.clear()
+        this.authService.checkUserToken();
+    }
+
+    ngOnInit() {
+        const that = this;
+        this.dataService.getData()
+            .then(function (response) {
+                that.userData = response;
+                that.userFullname = that.userData.firstname.concat(' ', that.userData.lastname);
+            });
+    }
 
 }
