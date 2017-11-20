9ee86bbcd393cf3ec8f4c826bf7f73f3ad75ef98
Panagiotis Dorgiakis <p.dorgiak@gmail.com>
2017-11-20 19:23:22 +0200

    Added the new url path to the component. Also the user can go to either, /main/profile or /main/home but not to /main. When he goes to /main he gets redirected to /main/home.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,6 +5,7 @@ import { SignUpComponent } from "./authentication/sign-up/sign-up.component"
 import { SignInComponent } from "./authentication/sign-in/sign-in.component"
 import {ProfileComponent} from "./main/profile/profile.component";
 import { MainComponent} from "./main/main.component";
+import {HomeComponent} from "./main/home/home.component";
 
 const APP_ROUTES: Routes = [
     {path: '', redirectTo: '/auth/sign-in', pathMatch: 'full' },
@@ -13,10 +14,10 @@ const APP_ROUTES: Routes = [
         {path: 'sign-in', component: SignInComponent },
         {path: 'sign-up', component: SignUpComponent }
     ]},
-
     {path: 'main', component: MainComponent,
     children: [
-        {path: 'profile', component: ProfileComponent}
+        {path: 'profile', component: ProfileComponent},
+        {path: 'home', component: HomeComponent}
     ]}
 ];
 
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthenticationService} from "../authentication.service";
-
+import {Router} from "@angular/router";
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-
-  constructor(private authService: AuthenticationService) {
-      authService.checkUserToken();
+  constructor(private authService: AuthenticationService,
+              private router: Router) {
+      router.navigateByUrl('/main/home');
   }
 
   ngOnInit() {
