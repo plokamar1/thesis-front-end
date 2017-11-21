31e4bcef7fdd678d8c7591795a9b95c417f48f8e
Panagiotis Dorgiakis <p.dorgiak@gmail.com>
2017-11-21 10:09:24 +0200

    Initialized the rss-reader component

---
+++ src/app/main/home/rss-reader/rss-reader.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-rss-reader',
+  templateUrl: './rss-reader.component.html',
+  styleUrls: ['./rss-reader.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class RssReaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
