a8e1f33932acc9d588f51d4aec757c379020a3dd
Panagiotis Dorgiakis <p.dorgiak@gmail.com>
2017-11-21 21:05:35 +0200

    So here is one of the 3 or 4 tabs that will be shown in the main page. The Rss reader. The user provides the url and i get to show the reader the news using the material panels. I need todo some error handling. In the future the visited rss urls will be saved for each user.

--- src/app/main/home/rss-reader/rss-reader.component.ts
+++ src/app/main/home/rss-reader/rss-reader.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import {RssReaderService} from "./rss-reader.service";
+import {Form} from "@angular/forms";
 
 @Component({
   selector: 'app-rss-reader',
@@ -8,15 +9,29 @@ import {RssReaderService} from "./rss-reader.service";
   encapsulation: ViewEncapsulation.None
 })
 export class RssReaderComponent implements OnInit {
+    private rssUrl: string;
+    private hidden = false;
+    private feedArray: any;
+    private mediaDescription: any;
+  constructor(private rssService: RssReaderService) {
 
-  constructor(private rssService: RssReaderService) { }
-
-  getRss(){
-     this.rssService.getJson()
-         .subscribe(function (response) {
-             console.log(response);
-         });
   }
+  receiveRss(){
+      this.hidden = true;
+      this.rssService.getJson(this.rssUrl)
+          .subscribe(data=> {
+              this.hidden=false;
+              console.log(data);
+              if(data.status === 'ok'){
+                  this.mediaDescription = data.feed;
+                  this.feedArray = data.items;
+              }
+
+          },error=>{
+              console.error(error);
+          });
+  }
+
   ngOnInit() {
   }
 
--- src/app/main/home/rss-reader/rss-reader.service.ts
+++ src/app/main/home/rss-reader/rss-reader.service.ts
@@ -1,7 +1,6 @@
 import {Injectable} from '@angular/core';
 import {Http, Response} from "@angular/http";
 import {Observable} from "rxjs/Observable";
-import {encode} from "punycode";
 
 @Injectable()
 export class RssReaderService {
@@ -9,9 +8,10 @@ export class RssReaderService {
     constructor(private http: Http) {
     }
 
-    getJson() {
+    getJson(rssUrl:string) {
         //the feed url gets escaped
-        const url = encodeURIComponent('http://rss.cnn.com/rss/edition.rss');
+        /*const url = encodeURIComponent('http://rss.cnn.com/rss/edition.rss');*/
+        const url = encodeURIComponent(rssUrl);
         //we build the url to call
         const link = 'https://rss2json.com/api.json?rss_url='.concat(url);
         return this.http.get(link)
