0447fb69052fb73e4280afaee5a5f10279a9edc2
Panagiotis Dorgiakis <p.dorgiak@gmail.com>
2017-11-24 18:30:22 +0200

    Again when the user resizes the browser window the height changes for the components as well

--- src/app/main/home/email-reader/email-reader.component.ts
+++ src/app/main/home/email-reader/email-reader.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DataService} from '../../../data.service';
 
 @Component({
-  selector: 'app-email-reader',
-  templateUrl: './email-reader.component.html',
-  styleUrls: ['./email-reader.component.css']
+    selector: 'app-email-reader',
+    templateUrl: './email-reader.component.html',
+    styleUrls: ['./email-reader.component.css']
 })
 export class EmailReaderComponent implements OnInit {
+    containerHeight: string;
 
-  constructor() { }
+    constructor(private dataService: DataService) {
+        this.containerHeight = (window.screen.height * 0.85) + 'px';
+    }
+    onResize($event) {
+        this.containerHeight = ($event.target.innerHeight * 0.85) + 'px';
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
