4894c1f77f5ca426674e718ecc50d6c46405831a
Panagiotis Dorgiakis <p.dorgiak@gmail.com>
2017-11-30 14:46:32 +0200

    the email reader will use the same css as the rss-reader that's why i imported it. I will probably use the css for all of the components inside home component from home component's css. Also the scrollOptions are for the malihu custom scrollbar

--- src/app/main/home/email-reader/email-reader.component.ts
+++ src/app/main/home/email-reader/email-reader.component.ts
@@ -1,17 +1,27 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {DataService} from '../../../data.service';
+import {AuthenticationService} from "../../../authentication.service";
 
 @Component({
     selector: 'app-email-reader',
     templateUrl: './email-reader.component.html',
-    styleUrls: ['./email-reader.component.css']
+    styleUrls: ['../rss-reader/rss-reader.component.css','./email-reader.component.css']
 })
-export class EmailReaderComponent implements OnInit {
+export class EmailReaderComponent implements OnInit, AfterViewInit {
+    isConnected :boolean = true;
+    scrollOptions = {axis: 'y', theme: 'minimal-dark', scrollButtons: {enable: true}};
+    scrollOptions2 = {axis: 'x', theme: 'minimal-dark', scrollButtons: {enable: true}};
     containerHeight: string;
-
-    constructor(private dataService: DataService) {
+    emails = [];
+    constructor(public dataService: DataService,
+                private authService: AuthenticationService) {
         this.containerHeight = (window.screen.height * 0.85) + 'px';
     }
+    showData(){
+
+        }
+    }
+
     onResize($event) {
         this.containerHeight = ($event.target.innerHeight * 0.85) + 'px';
     }
