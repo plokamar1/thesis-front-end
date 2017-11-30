56df7b513058658a9041c6baef9cf1e033c7d0ac
Panagiotis Dorgiakis <p.dorgiak@gmail.com>
2017-11-30 15:33:38 +0200

    Also in the email-reader i use the expansion panels to show the user his emails and the malihu scrollbar to check them out. I made an ng-template to show in case the user doesnt have a connected google account.

--- src/app/main/home/email-reader/email-reader.component.ts
+++ src/app/main/home/email-reader/email-reader.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, OnInit} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import {DataService} from '../../../data.service';
 import {AuthenticationService} from "../../../authentication.service";
 
@@ -7,26 +7,23 @@ import {AuthenticationService} from "../../../authentication.service";
     templateUrl: './email-reader.component.html',
     styleUrls: ['../rss-reader/rss-reader.component.css','./email-reader.component.css']
 })
-export class EmailReaderComponent implements OnInit, AfterViewInit {
+export class EmailReaderComponent {
     isConnected :boolean = true;
     scrollOptions = {axis: 'y', theme: 'minimal-dark', scrollButtons: {enable: true}};
     scrollOptions2 = {axis: 'x', theme: 'minimal-dark', scrollButtons: {enable: true}};
     containerHeight: string;
     emails = [];
+
     constructor(public dataService: DataService,
                 private authService: AuthenticationService) {
         this.containerHeight = (window.screen.height * 0.85) + 'px';
     }
     showData(){
-
-        }
     }
 
     onResize($event) {
         this.containerHeight = ($event.target.innerHeight * 0.85) + 'px';
     }
 
-    ngOnInit() {
-    }
 
 }
