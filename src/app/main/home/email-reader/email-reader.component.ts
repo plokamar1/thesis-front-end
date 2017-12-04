5d0dc20737bd02924777208a1cfdc33e1252ffcd
Panagiotis Dorgiakis <p.dorgiak@gmail.com>
2017-12-04 12:38:24 +0200

    When the component load it is checked if the user has connected a google account. If he hasnt then he is shown an ng-template where he will press to add an account.

--- src/app/main/home/email-reader/email-reader.component.ts
+++ src/app/main/home/email-reader/email-reader.component.ts
@@ -1,29 +1,39 @@
-import { Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {DataService} from '../../../data.service';
 import {AuthenticationService} from "../../../authentication.service";
+import {ActivatedRoute, Params} from "@angular/router";
 
 @Component({
     selector: 'app-email-reader',
     templateUrl: './email-reader.component.html',
-    styleUrls: ['../rss-reader/rss-reader.component.css','./email-reader.component.css']
+    styleUrls: ['../rss-reader/rss-reader.component.css', './email-reader.component.css']
 })
-export class EmailReaderComponent {
-    isConnected :boolean = true;
+export class EmailReaderComponent implements OnInit,AfterViewInit {
+    isConnected: boolean = true;
     scrollOptions = {axis: 'y', theme: 'minimal-dark', scrollButtons: {enable: true}};
     scrollOptions2 = {axis: 'x', theme: 'minimal-dark', scrollButtons: {enable: true}};
     containerHeight: string;
-    emails = [];
 
     constructor(public dataService: DataService,
-                private authService: AuthenticationService) {
+                private authService: AuthenticationService,
+                private route: ActivatedRoute) {
         this.containerHeight = (window.screen.height * 0.85) + 'px';
-    }
-    showData(){
+
+
     }
 
     onResize($event) {
         this.containerHeight = ($event.target.innerHeight * 0.85) + 'px';
     }
+    ngAfterViewInit() {
+        if (this.dataService.checkIfConnected('google')) {
+            this.dataService.getMail();
+        } else {
+            this.isConnected = false;
+        }
+    }
 
+    ngOnInit() {
 
+    }
 }
