6eb4d9348e0fba689c5c4c2ea3360c5cc575cc64
klianos <klianos@singularlogic.eu>
2017-12-04 15:57:11 +0200

    remove apis-resolver

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,25 +1,28 @@
 import {RouterModule, Routes} from '@angular/router';
 
-import { AuthenticationComponent} from './authentication/authentication.component';
-import { SignUpComponent } from './authentication/sign-up/sign-up.component'
-import { SignInComponent } from './authentication/sign-in/sign-in.component'
+import {AuthenticationComponent} from './authentication/authentication.component';
+import {SignUpComponent} from './authentication/sign-up/sign-up.component'
+import {SignInComponent} from './authentication/sign-in/sign-in.component'
 import {ProfileComponent} from './main/profile/profile.component';
-import { MainComponent} from './main/main.component';
+import {MainComponent} from './main/main.component';
 import {HomeComponent} from './main/home/home.component';
-import {ApisResolverService} from "./apis-resolver.service";
 
 const APP_ROUTES: Routes = [
-    {path: '', redirectTo: '/auth/sign-in', pathMatch: 'full' },
-    {path: 'auth', component: AuthenticationComponent,
-    children: [
-        {path: 'sign-in', component: SignInComponent },
-        {path: 'sign-up', component: SignUpComponent }
-    ], resolve:{ response: ApisResolverService}},
-    {path: 'main', component: MainComponent,
-    children: [
-        {path: 'profile', component: ProfileComponent},
-        {path: 'home', component: HomeComponent}
-    ], resolve: { response: ApisResolverService}}
+    {path: '', redirectTo: '/auth/sign-in', pathMatch: 'full'},
+    {
+        path: 'auth', component: AuthenticationComponent,
+        children: [
+            {path: 'sign-in', component: SignInComponent},
+            {path: 'sign-up', component: SignUpComponent}
+        ]
+    },
+    {
+        path: 'main', component: MainComponent,
+        children: [
+            {path: 'profile', component: ProfileComponent},
+            {path: 'home', component: HomeComponent}
+        ]
+    }
 ];
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
